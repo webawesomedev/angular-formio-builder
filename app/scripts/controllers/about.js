02fba9fd3a48ee7936420c1b4f5dd9bef38a6243
Ken Abe <kabe@altarepublishing.com>
2018-04-09 09:29:05 -0800

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularFormioBuilderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularFormioBuilderApp
+ */
+angular.module('angularFormioBuilderApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularFormioBuilderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularFormioBuilderApp
+ */
+angular.module('angularFormioBuilderApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
