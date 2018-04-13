20edfd3dee270b1ce816840238fc5ed07f66f010
Ken Abe <kabe@altarepublishing.com>
2018-04-13 04:50:27 -0800

    CKEditor Integrates

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -47,6 +47,7 @@ module.exports = function(config) {
       'bower_components/ng-tags-input/ng-tags-input.js',
       'bower_components/ng-formio-builder/dist/ngFormBuilder.js',
       'bower_components/ng-json-explorer/dist/angular-json-explorer.js',
+      'bower_components/ckeditor/ckeditor.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
